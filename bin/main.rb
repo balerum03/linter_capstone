e59e24365de54df6b670875d2dbcb09678b5f082
Adan Fernandez <adafer123.angra@gmail.com>
2020-09-01 01:00:15 -0500

    fixed some bugs with the counters

--- bin/main.rb
+++ bin/main.rb
@@ -1,27 +1,43 @@
-one_array = []
-two_array = []
-keywords = %w[class def if elsif for while]
+require 'colorize'
+open_array = []
+close_array = []
+hello = /\[/
+hello2 = /\]/
 my_file = File.open("tester.rb", "r")
   my_file.readlines().each_with_index do |lines, line_index|
-    if lines.match('do').to_s == "do"
-      one_array << line_index
+    if lines.match(hello)
+      open_array << line_index
     end
-    if lines.match('end').to_s == "end"
-      two_array << line_index
+    if lines.match(hello2)
+      close_array << line_index
     end
   end
 
-  p one_array
-  p two_array
+  close_counter = open_array + close_array
 
-if one_array.length > two_array.length
-  total_missed = one_array.length - two_array.length
-  puts "You are missing #{total_missed} end"
-elsif one_array.length < two_array.length
-  total_missed = two_array.length - one_array.length
-  puts "You are missing #{total_missed} do"
-elsif one_array.length == two_array.length
-  puts 'good job dude'
-end
+  if open_array.length > close_array.length
+    close_counter = close_counter - close_array
+  elsif open_array.length < close_array.length
+    open_counter = close_counter - open_array
+  end 
+
+    close_counter.uniq!
+    open_counter = close_counter
+    close_counter = close_counter - close_array
+    open_counter = open_counter - open_array
+
+  if close_counter.empty? && open_counter.empty?
+    puts "Everything looks good".colorize(:green)
+  end
+  if !close_counter.nil?
+    close_counter.each do |error_index|
+      puts "Check this line #{error_index + 1}, you could be missing something".colorize(:red)
+    end
+  end
+  if !open_counter.nil?
+    open_counter.each do |error_index|
+      puts "Check this line #{error_index + 1}, you could be missing something".colorize(:red)
+    end
+  end
 
 my_file.close()
--- bin/tester.rb
+++ bin/tester.rb
@@ -1,11 +1,11 @@
-def my_method do
+def my_method (hola, name) do
   puts "hello this is just a test"
 end
 
-def second_method(has, the) do
+def second_method [has, the] do
   puts "this is another method for testing"
 end
 
-class Test_class do
+class Test_class {hello} do
   my_name = 'adan'
 end
