3d27399ba9e224a5aecce656ebe0bba0d75dce88
Adan Fernandez <adafer123.angra@gmail.com>
2020-09-03 18:59:08 -0500

    eliminated unnecessary methods and simplified code over all

--- lib/checker.rb
+++ lib/checker.rb
@@ -4,32 +4,21 @@ class Checker
 
   def initialize(filepath)
     @my_file = filepath
-    @open_index = []
-    @close_index = []
+    @error_index = []
   end
 
   def file_checker(opener, closer)
-    @open_index = []
-    @close_index = []
+    @error_index = []
     opened = File.open(@my_file)
     opened.readlines.each_with_index do |lines, line_index|
-      @open_index << line_index if lines.match(opener)
-      @close_index << line_index if lines.match(closer)
+      if lines.rindex(opener).to_i > lines.rindex(closer).to_i
+        @error_index << line_index
+      end
+      if lines.rindex(opener).nil? && !lines.rindex(closer).nil?
+        @error_index << line_index
+      end
     end
     opened.close
-    [@open_index, @close_index]
-  end
-
-  def file_analizer(arg_to_check1, arg_to_check2)
-    @receiver = file_checker(arg_to_check1, arg_to_check2)
-    open_array = @receiver[0]
-    close_array = @receiver[1]
-    close_counter = open_array + close_array
-    close_counter -= close_array if open_array.length > close_array.length
-    close_counter.uniq!
-    open_counter = close_counter
-    close_counter -= close_array
-    open_counter -= open_array
-    [open_counter, close_counter]
+    @error_index
   end
 end
