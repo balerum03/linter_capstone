6bd4bf70f386405182867ff9a8839b6396a7fab7
Adan Fernandez <adafer123.angra@gmail.com>
2020-09-03 23:44:24 -0500

    added aditional line to checker.rb in order to test additional possible errors

--- lib/checker.rb
+++ lib/checker.rb
@@ -11,10 +11,11 @@ class Checker
     @error_index = []
     opened = File.open(@my_file)
     opened.readlines.each_with_index do |lines, line_index|
+      @error_index << line_index if lines.scan(opener).count != lines.scan(closer).count
       @error_index << line_index if lines.rindex(opener).to_i > lines.rindex(closer).to_i
       @error_index << line_index if lines.rindex(opener).nil? && !lines.rindex(closer).nil?
     end
     opened.close
-    @error_index
+    @error_index.uniq
   end
 end
